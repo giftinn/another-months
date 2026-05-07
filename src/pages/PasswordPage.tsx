c443e87bc0f1a548e3c59156073e68e36f8ec417
giftin.id <calistanafisaa@gmail.com>
2026-05-07 21:16:43 +0700

    Update PasswordPage.tsx

--- src/pages/PasswordPage.tsx
+++ src/pages/PasswordPage.tsx
@@ -10,7 +10,7 @@ const PasswordPage: React.FC<PasswordPageProps> = ({ onNext }) => {
   const [password, setPassword] = useState('');
   const [showError, setShowError] = useState(false);
   const [showSuccess, setShowSuccess] = useState(false);
-  const correctPassword = '0805';
+  const correctPassword = '0705';
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
